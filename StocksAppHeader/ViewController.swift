2a3369c6f9ec7770ee19fba49f14267a2c6fc94b
Ian Schoenrock <ischoenrock@lumina247.net>
2019-12-31 17:01:57 -0500

    Initial Commit

---
+++ StocksAppHeader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StocksAppHeader
+//
+//  Created by ian schoenrock on 12/31/19.
+//  Copyright © 2019 ian schoenrock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
